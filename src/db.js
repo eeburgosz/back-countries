5a5f067a89c1e6be397e89ad8e3f9323350015a4
eeburgosz <hdst.bur@gmail.com>
2023-09-10 14:14:55 -0300

    dontenv modificado - info db

--- src/db.js
+++ src/db.js
@@ -3,9 +3,9 @@ const CountryModel = require('./models/Country');
 const ActivityModel = require('./models/Activity');
 
 require('dotenv').config();
-const { DB_HOST, DB_USER, DB_PASSWORD } = process.env;
+const { PGHOST, DB_USER, PGPASSWORD, PGPORT, PGDATABASE } = process.env;
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/countries`, { logging: false });
+const sequelize = new Sequelize(`postgres://${DB_USER}:${PGPASSWORD}@${PGHOST}:${PGPORT}/${PGDATABASE}`, { logging: false });
 
 CountryModel(sequelize);
 ActivityModel(sequelize);
