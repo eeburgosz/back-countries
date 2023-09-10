37a668dd185e31379d71c04020a3687184acb873
eeburgosz <hdst.bur@gmail.com>
2023-09-10 20:05:48 -0300

    Ruta de prueba 10

--- src/db.js
+++ src/db.js
@@ -6,21 +6,25 @@ require('dotenv').config();
 const { DB_HOST, DB_USER, DB_PASSWORD, DATABASE_URL } = process.env;
 
 // const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/countries`, { logging: false });
-const sequelize = new Sequelize(DATABASE_URL, {
-   logging: false, dialect: "postgres", native: false, ssl: true,
-   // pool: {
-   //    max: 3,
-   //    min: 1,
-   //    idle: 10000
-   // },
-   dialectOptions: {
-      ssl: {
-         require: true,
-         rejectUnauthorized: false
+const sequelize = new Sequelize(DATABASE_URL,
+   {
+      logging: false,
+      dialect: "postgres",
+      native: false,
+      ssl: true,
+      pool: {
+         max: 3,
+         min: 1,
+         idle: 10000
       },
-      keepAlive: true
-   }
-});
+      dialectOptions: {
+         ssl: {
+            require: true,
+            rejectUnauthorized: false
+         },
+         // keepAlive: true
+      }
+   });
 
 CountryModel(sequelize);
 ActivityModel(sequelize);
