e5d4efd777d7912e263650742bd589dc798c93a1
eeburgosz <hdst.bur@gmail.com>
2023-09-10 17:13:44 -0300

    Ruta de prueba

--- src/app.js
+++ src/app.js
@@ -15,14 +15,14 @@ const pool = new pg.Pool({
 
 app.use(morgan("dev"));
 app.use(express.json());
-// app.get('/hola', async (req, res) => {
-//    try {
-//       const result = await pool.query('SELECT NOW()');
-//       res.json(result.rows[0]);
-//    } catch (error) {
-//       res.status(404).json({ error: error.message });
-//    }
-// });
+app.get('/hola', async (req, res) => {
+   try {
+      const result = await pool.query('SELECT NOW()');
+      res.json(result.rows[0]);
+   } catch (error) {
+      res.status(404).json({ error: error.message });
+   }
+});
 app.use(mainRouter);
 
 module.exports = app;
