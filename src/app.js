5c410783855fc0beb0ca124eb2806b4c0ac9642d
eeburgosz <hdst.bur@gmail.com>
2023-09-10 19:46:58 -0300

    Ruta de prueba 5

--- src/app.js
+++ src/app.js
@@ -1,28 +1,11 @@
 const express = require('express');
 const morgan = require("morgan");
 const mainRouter = require("./routes");
-const pg = require('pg');
-require('dotenv').config();
 
-const { DATABASE_URL } = process.env;
 const app = express();
-
-/* const pool =  */new pg.Pool({
-   connectionString: `${DATABASE_URL}`,
-   // ssl: true,
-});
-
 
 app.use(morgan("dev"));
 app.use(express.json());
-// app.get('/hola', async (req, res) => {
-//    try {
-//       const result = await pool.query('SELECT NOW()');
-//       res.json(result.rows[0]);
-//    } catch (error) {
-//       res.status(404).json({ error: error.message });
-//    }
-// });
 app.use(mainRouter);
 
 module.exports = app;
--- src/db.js
+++ src/db.js
@@ -3,9 +3,12 @@ const CountryModel = require('./models/Country');
 const ActivityModel = require('./models/Activity');
 
 require('dotenv').config();
-const { DB_HOST, DB_USER, DB_PASSWORD } = process.env;
+const { DB_HOST, DB_USER, DB_PASSWORD, DATABASE_URL } = process.env;
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/countries`, { logging: false });
+// const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/countries`, { logging: false });
+const sequelize = new Sequelize(DATABASE_URL, {
+   logging: false, dialect: "postgres", native: false,
+});
 
 CountryModel(sequelize);
 ActivityModel(sequelize);
