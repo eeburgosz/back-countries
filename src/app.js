eb206b9dadf6040ea76ae36f7e7e801306b3a4ef
eeburgosz <hdst.bur@gmail.com>
2023-09-10 15:10:43 -0300

    Probando configuración

--- src/app.js
+++ src/app.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const morgan = require("morgan");
 const mainRouter = require("./routes");
+const pg = require('pg');
+require('dotenv').config();
 
+const { DATABASE_URL } = process.env;
 const app = express();
+
+/* const pool =  */new pg.Pool({
+   connectionString: `${DATABASE_URL}`
+});
 
 app.use(morgan("dev"));
 app.use(express.json());
