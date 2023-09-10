87370e9686be63e38a8b355425bb8d88c2aff801
eeburgosz <hdst.bur@gmail.com>
2023-09-10 15:25:33 -0300

    Ruta de prueba

--- src/app.js
+++ src/app.js
@@ -2,17 +2,24 @@ const express = require('express');
 const morgan = require("morgan");
 const mainRouter = require("./routes");
 const pg = require('pg');
+// const { SELECT } = require("sequelize/types/query-types");
 require('dotenv').config();
 
 const { DATABASE_URL } = process.env;
 const app = express();
 
-/* const pool =  */new pg.Pool({
-   connectionString: `${DATABASE_URL}`
+const pool = new pg.Pool({
+   connectionString: `${DATABASE_URL}`,
+   ssl: true,
 });
+
 
 app.use(morgan("dev"));
 app.use(express.json());
+app.get('/', async (req, res) => {
+   const result = await pool.query('SELECT NOW()');
+   return res.json(result.rows[0]);
+});
 app.use(mainRouter);
 
 module.exports = app;
