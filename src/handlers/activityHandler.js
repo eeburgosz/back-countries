39d40288d64249a40b679886e76a1232e8a17adc
eeburgosz <hdst.bur@gmail.com>
2023-09-10 22:23:50 -0300

    Configuración db para  prod-dev 9

--- src/handlers/activityHandler.js
+++ src/handlers/activityHandler.js
@@ -4,7 +4,7 @@ const createActivityHandler = async (req, res) => {
    const { countryId, activityName, difficulty, duration, season } = req.body;
    try {
       const newActivity = await createActivity(countryId, activityName, difficulty, duration, season);
-      res.status(201).send(newActivity);
+      res.status(201).json(newActivity);
    } catch (error) {
       res.status(400).json({ error: error.message });
    }
