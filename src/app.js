e901efe6f7b20eafb5ae05838413744746348cb9
eeburgosz <hdst.bur@gmail.com>
2023-09-10 22:11:06 -0300

    Configuración db para  prod-dev 8

--- src/app.js
+++ src/app.js
@@ -6,6 +6,13 @@ const app = express();
 
 app.use(morgan("dev"));
 app.use(express.json());
+app.use((req, res, next) => {
+   res.header('Access-Control-Allow-Origin', '*');
+   res.header('Access-Control-Allow-Credentials', 'true');
+   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+   next();
+});
 app.use(mainRouter);
 
 module.exports = app;
