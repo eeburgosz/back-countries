d9e17510647a3821fc77b6f5bc872903849c31fd
eeburgosz <hdst.bur@gmail.com>
2023-09-13 19:41:40 -0300

    Instalación de CORS

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const morgan = require("morgan");
 const mainRouter = require("./routes");
+const cors = require("cors");
 
 const app = express();
 
-app.use(morgan("dev"));
 app.use(express.json());
+app.use(cors());
+app.use(morgan("dev"));
 app.use(mainRouter);
 
 module.exports = app;
