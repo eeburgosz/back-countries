799da1b1ab7dcd87ca24ff776cff17ac6400e7e5
eeburgosz <hdst.bur@gmail.com>
2023-09-24 18:35:28 -0300

    Carga de info

--- src/controllers/activityControllers.js
+++ src/controllers/activityControllers.js
@@ -1,4 +1,11 @@
 const { Activity, Country } = require("../db");
+const { infoCountriesApi } = require("../utils/api");
+
+(async () => {
+   const countries = await infoCountriesApi();
+   const db = await Country.findAll();
+   !db ? await Country.bulkCreate(countries) : null;
+})();
 
 const createActivity = async (countryId, activityName, difficulty, duration, season) => {
    const exist = await Activity.findOne({
